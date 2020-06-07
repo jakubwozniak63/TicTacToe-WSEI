06cfed5ee5777cbe0164a0a1ebb05efa4000f409
jakub.wozniak63 <jakub.wozniak63@gmail.com>
2020-06-07 18:09:26 +0200

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Windows;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Private Members
+        /// <summary>
+        /// Holds the current results of cells in the active game
+        /// </summary>
+        private MarkType[] mResults;
+
+        /// <summary>
+        /// True if it is player 1's turn (X) or player 2's turn (0)
+        /// </summary>
+        private bool mPlayer1Turn;
+
+        /// <summary>
+        /// True if game has ended
+        /// </summary>
+        private bool mGameEnded;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NewGame()
+        }
+
+        #endregion
+
+        private void NewGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
